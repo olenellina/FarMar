42f92a7848f3e3793d3e3b2ed55b7a86fc9e0971
Melissa Rodriguez <olenellina@gmail.com>
2016-09-09 13:52:11 -0700

    adding the file for the new Shared class

---
+++ lib/shared.rb
@@ -0,0 +1,23 @@
+# Shared Methods Class
+
+require_relative '../far_mar'
+
+class FarMar::Shared
+
+  # self.find will return the object associated with an id passed to it by the calling class. If the id cannot be found, an ArgumentError is raised.
+  def self.find(find_id)
+    all = self.all
+    obj = nil
+    all.each do |element|
+      if element.id == find_id
+        obj = element
+      end
+    end
+    if obj.nil?
+      raise ArgumentError.new("This id cannot be found")
+    else
+      return obj
+    end
+  end
+
+end
